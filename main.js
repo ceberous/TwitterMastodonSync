421a6f0f9ffe38fe4f3239c8f867f50d20df8133
morpheous <cerbus.collin@gmail.com>
2018-01-12 17:07:28 -0800

    6

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function POST_SLACK_ERROR( wStatus ) {
 				try { wStatus = wStatus.toString(); }
 				catch( e ) { wStatus = e; }
 			}
-			await SLACK_POST_MESSAGE.post( wStatus , "#msync-err" );
+			await SLACK_POST_MESSAGE( wStatus , "#msync-err" );
 			resolve();
 		}
 		catch( error ) { console.log( error ); reject( error ); }
